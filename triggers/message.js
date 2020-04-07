563f9734e8f82e9c908404514a7c25a56a35da3b
JosephJvB <joevanbo@pm.me>
2020-04-07 21:39:27 +1200

    remove meddling

--- triggers/message.js
+++ triggers/message.js
@@ -6,20 +6,16 @@ const montage = require('./lib/montage')
 const toS3 = require('./lib/s3')
 
 let socketId // send updates to request socket
-let query
 
 exports.handler = async (event, context) => {
     try {
         const data = JSON.parse(event.Records[0].body)
         socketId = data.socketId
-        query = data.q
         const paths = await downloadCropSaveRecursive(data.urls)
         const finalBuffer = await montage(paths)
-        const s3Url = await toS3(finalBuffer, `${query}-montage.jpg`)
+        const s3Url = await toS3(finalBuffer, `${data.q}-montage.jpg`)
         console.log('DONE DONE DONE', s3Url)
         await loaded(s3Url)
-        socketId = null
-        inProgressId = null
     } catch (e) {
         console.error('ERROR', e)
     }
@@ -33,13 +29,9 @@ async function downloadCropSaveRecursive (urls, paths = [], id = 0) {
     }
     console.log(id, ':', u)
     const p = `/tmp/${query}-${id}-${socketId}.jpg`
-    if(!fs.existsSync(p)) {
-        const fullBuff = await download(u)
-        const cropBuff = await crop(fullBuff)
-        fs.writeFileSync(p, cropBuff)
-    } else {
-        console.log('already have that img')
-    }
+    const fullBuff = await download(u)
+    const cropBuff = await crop(fullBuff)
+    fs.writeFileSync(p, cropBuff)
     paths.push(p)
     return Promise.all([
         downloadCropSaveRecursive(urls, paths, id+1),
